acaf5794228db378909a82c360e10c4c57635bd6
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-01-22 15:53:29 -0600

    Add About page and update Navbar links

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+const About = () => {
+	return (
+		<main className="flex flex-col items-center justify-between min-h-screen p-24">
+
+		</main>
+	);
+};
+
+export default About;
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,30 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
-	return (
+    return (
         <header className="flex items-center h-16 px-8 bg-transparent shadow-md lg:px-12">
-            <a className="w-[10vw] h-[5rem] relative" href="./home.html" rel="ugc">
+            <Link className="w-[10vw] h-[5rem] relative" href="/" rel="ugc">
                 <Image src="/kwlogo_trans.webp" alt="Keller Williams Logo" className="object-contain" fill />
-            </a>
+            </Link>
             <nav className="flex gap-4 ml-auto sm:gap-6">
-                <a className="text-sm font-medium hover:underline underline-offset-4" href="./home.html" rel="ugc">
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/" rel="ugc">
                     Home
-                </a>
-                <a className="text-sm font-medium hover:underline underline-offset-4" href="./listings.html" rel="ugc">
+                </Link>
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/listings" rel="ugc">
                     Listings
-                </a>
-                <a className="text-sm font-medium hover:underline underline-offset-4" href="./open-houses.html" rel="ugc">
+                </Link>
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/open-houses" rel="ugc">
                     Open Houses
-                </a>
-                <a className="text-sm font-medium hover:underline underline-offset-4" href="./about.html" rel="ugc">
+                </Link>
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/about" rel="ugc">
                     About
-                </a>
-                <a className="text-sm font-medium hover:underline underline-offset-4" href="./contact.html" rel="ugc">
+                </Link>
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/contact" rel="ugc">
                     Contact
-                </a>
+                </Link>
             </nav>
         </header>
-	);
+    );
 };
 
 export default Navbar;
