a9cf21a6b48724a52eea3906fb4a1876cca869a5
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-01-30 18:07:21 -0600

    Remove react-responsive package

--- components/Navbar/NavbarClient.js
+++ components/Navbar/NavbarClient.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect, useRef, useCallback } from "react";
-import { useMediaQuery } from "react-responsive";
 import Image from "next/image";
 import Link from "next/link";
 import { debounce } from "./debounce";
