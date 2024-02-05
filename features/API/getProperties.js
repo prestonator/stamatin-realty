e56e507466b1b9133b7d7c9f0aa252bdee5c2f7d
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-02-04 20:09:47 -0600

    Remove unused imports and commented code

--- features/API/getProperties.js
+++ features/API/getProperties.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-import fs from "fs/promises";
-import path from "path";
 
 const axiosFetch = axios.create({
 	baseURL: "https://realty-in-us.p.rapidapi.com/",
@@ -21,28 +19,4 @@ export const getProperties = async () => {
 		},
 	});
 	return data.data.forSale.results;
-};
-
-const getPropertyDetails = async () => {
-	try {
-		const properties = await getProperties(); // This calls your getProperties function from above
-		const detailPromises = properties.map((property) => {
-			// Destructure the needed parameters from each property
-			const { property_id, listing_id } = property;
-			return axiosFetch
-				.get("/properties/v3/detail", {
-					params: { property_id, listing_id },
-				})
-				.then((response) => response.data); // Return the detailed data
-		});
-
-		// Wait for all the detail calls to finish
-		const detailedProperties = await Promise.all(detailPromises);
-
-		// Now you have an array of detailed properties
-		return detailedProperties;
-	} catch (error) {
-		console.error("Error fetching properties with details:", error);
-		throw error;
-	}
 };
