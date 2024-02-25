8675e08b433c4c6faeb4702141b9ddfe1fa84fe9
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-02-25 16:41:24 -0600

    Refactor MapView component and update import statements

--- app/listings/MapView.jsx
+++ app/listings/MapView.jsx
@@ -1,39 +1,45 @@
 "use client";
 import React, { useState } from "react";
 import {
-	GoogleMap,
-	LoadScript,
+	APIProvider,
+	Map,
 	Marker,
 	InfoWindow,
-} from "@react-google-maps/api";
-import { APIProvider } from "@vis.gl/react-google-maps";
+	AdvancedMarker,
+} from "@vis.gl/react-google-maps";
+import { BsHouse } from "react-icons/bs";
 
 const center = {
 	lat: 35.229956,
 	lng: -97.482395,
 };
 
-const googleMapsApiKey = process.env.MAPS_API_KEY;
-
 const MapView = ({ filteredProperties }) => {
 	const [selectedProperty, setSelectedProperty] = useState(null);
+	const googleMapsApiKey = "AIzaSyALR2pJSTn_4ISX2KXwsOoWUTSOEVmJisw";
+	const mapID = "d569390fab70022c";
 
 	return (
-		<LoadScript googleMapsApiKey="AIzaSyALR2pJSTn_4ISX2KXwsOoWUTSOEVmJisw">
-			<GoogleMap
-				mapContainerStyle={{ width: "100%", height: "400px" }}
-				center={center}
-				zoom={10}
+		<APIProvider apiKey={googleMapsApiKey}>
+			<Map
+				style={{ width: "100%", height: "400px" }}
+				defaultCenter={center}
+				defaultZoom={10}
+				mapId={mapID}
 			>
 				{filteredProperties.map((property) => (
-					<Marker
+					<AdvancedMarker
 						key={property.property_id}
 						position={{
 							lat: property.location.latitude,
 							lng: property.location.longitude,
 						}}
 						onClick={() => setSelectedProperty(property)}
-					/>
+					>
+						<div className="px-2 py-1 transition-all bg-white rounded-lg shadow-lg opacity-80 hover:opacity-100">
+							<p>${property.list_price.toLocaleString()}</p>
+						</div>
+					</AdvancedMarker>
 				))}
 
 				{selectedProperty && (
@@ -51,8 +57,8 @@ const MapView = ({ filteredProperties }) => {
 						</div>
 					</InfoWindow>
 				)}
-			</GoogleMap>
-		</LoadScript>
+			</Map>
+		</APIProvider>
 	);
 };
 
