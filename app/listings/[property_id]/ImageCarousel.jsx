96d8149e353322cd8747c3137065ed3b373e8c1b
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-02-26 15:08:14 -0600

    Add ImageCarousel component and update page.js

---
+++ app/listings/[property_id]/ImageCarousel.jsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SimpleSlider = (props) => {
+	const [nav1, setNav1] = useState(null);
+	const [nav2, setNav2] = useState(null);
+	let sliderRef1 = useRef(null);
+	let sliderRef2 = useRef(null);
+
+	useEffect(() => {
+		setNav1(sliderRef1);
+		setNav2(sliderRef2);
+	}, []);
+
+	const settingsOne = {
+		asNavFor: nav2,
+		fade: true,
+		arrows: false,
+	};
+
+	const settingsTwo = {
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		swipeToSlide: true,
+		asNavFor: nav1,
+		centerMode: true,
+		focusOnSelect: true,
+        arrows: false,
+	};
+
+	const { photos } = props;
+
+	return (
+		<div className="flex flex-col gap-4">
+			<Slider
+				asNavFor={nav2}
+				ref={(slider) => (sliderRef1 = slider)}
+				{...settingsOne}
+			>
+				{photos.map((photo) => (
+					<div className="!flex justify-center" key={photo.href}>
+						<Image
+							className="object-cover aspect-[2/1] rounded-3xl"
+							src={photo.href}
+							alt="{photo.description}"
+							width={1000}
+							height={500}
+						/>
+					</div>
+				))}
+			</Slider>
+			<Slider ref={(slider) => (sliderRef2 = slider)} {...settingsTwo}>
+				{photos.map((photo) => (
+					<div key={photo.href} className="">
+						<Image
+							className="rounded-2xl"
+							src={photo.href}
+							alt="{photo.description}"
+							width={200}
+							height={100}
+						/>
+					</div>
+				))}
+			</Slider>
+		</div>
+	);
+};
+
+export default SimpleSlider;
--- app/listings/[property_id]/page.js
+++ app/listings/[property_id]/page.js
@@ -1,5 +1,7 @@
 // app/listings/[property_id]/page.js
 import propertyDetails from "@/data/propertyDetails.json";
+import SimpleSlider from "./ImageCarousel";
+import { LuBed, LuBath, LuCar, LuRuler } from "react-icons/lu";
 
 export async function generateStaticParams() {
 	// Map through your listings to create params for each property
@@ -18,10 +20,77 @@ const Page = ({ params }) => {
 		return <div>Property not found</div>;
 	}
 
+	const { street_number, city, state_code, postal_code } = property.location;
+
+	const { list_price, photos, description } = property;
+
+	const { bath_count, bed_count, garage_count, sqft, property_description } =
+		description;
+
 	return (
-		<section>
-			<div></div>
-		</section>
+		<>
+			<section className="pt-32">
+				<div>
+					<SimpleSlider photos={photos} />
+				</div>
+			</section>
+			<section>
+				<div>
+					<h1>
+						{street_number}{" "}
+						<span>
+							{city}, {state_code} {postal_code}
+						</span>
+					</h1>
+					<p>${list_price.toLocaleString()}</p>
+				</div>
+			</section>
+			<section>
+				<h2>Home Details</h2>
+				<div className="grid grid-cols-4 gap-2 pt-4 pb-12">
+					<div className="flex flex-col items-center">
+						<span className="block text-gray-700 text-md">{bed_count} Bed</span>
+						<hr className="w-3/4 my-1" />
+						<LuBed className="w-8 h-8 text-gray-500" />
+					</div>
+					<div className="flex flex-col items-center">
+						<span className="block text-gray-700 text-md">
+							{bath_count} Bath(s)
+						</span>
+						<hr className="w-3/4 my-1" />
+						<LuBath className="w-8 h-8 text-gray-500" />
+					</div>
+					<div className="flex flex-col items-center">
+						<span className="block text-gray-700 text-md">
+							{garage_count} Parking Space
+						</span>
+						<hr className="w-3/4 my-1" />
+						<LuCar className="w-8 h-8 text-gray-500" />
+					</div>
+					<div className="flex flex-col items-center">
+						<span className="block text-gray-700 text-md">{sqft} sqft</span>
+						<hr className="w-3/4 my-1" />
+						<LuRuler className="w-8 h-8 text-gray-500" />
+					</div>
+				</div>
+			</section>
+			<section>
+				<h2>What is around this property?</h2>
+			</section>
+			<section>
+				<h2>Description</h2>
+				<p>{property_description}</p>
+			</section>
+			<section>
+				<h3>Similar Homes You May Like</h3>
+			</section>
+			<section>
+				<h4>View on Realtor.com</h4>
+			</section>
+			<section>
+				<h4>Connect with Alex Stamatin Now.</h4>
+			</section>
+		</>
 	);
 };
 
