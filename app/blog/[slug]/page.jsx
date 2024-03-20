7449b53841139f042035bb01abad3047f3054c98
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-03-20 16:19:16 -0500

    Add blog listing and individual blog page components

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,24 +1,57 @@
 // app/blog/[slug]/page.jsx
-import getPosts, { getPost } from "@/lib/get-posts";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
 import { PostBody } from "@/mdx/post-body";
 import { notFound } from "next/navigation";
 import LatestPosts from "./parts/latestPosts";
 import "./markdown.css";
 
 export async function generateStaticParams() {
-	const posts = await getPosts();
-	return posts.map((post) => ({ slug: post.slug }));
+	const files = fs.readdirSync(path.join("blogs"));
+
+	const paths = files.map((filename) => ({
+		slug: filename.replace(".mdx", ""),
+	}));
+
+	return paths;
+}
+
+function getPost({ slug }) {
+	const markdownFile = fs.readFileSync(
+		path.join("blogs", slug + ".mdx"),
+		"utf-8"
+	);
+
+	const { data: frontMatter, content } = matter(markdownFile);
+
+	return {
+		frontMatter,
+		slug,
+		content,
+	};
+}
+
+export async function generateMetadata({ params }) {
+	const blog = getPost(params);
+
+	return {
+		title: blog.frontMatter.title,
+		description: blog.frontMatter.description,
+	};
 }
 
 export default async function PostPage({ params }) {
-	const post = await getPost(params.slug);
+	const props = getPost(params);
 	// notFound is a Next.js utility
-	if (!post) return notFound();
+	if (!props) return notFound();
 	// Pass the post contents to MDX
 	return (
 		<main className="flex flex-col items-center justify-between px-4 py-6 mx-auto max-w-7xl">
 			<article>
-				<PostBody>{post?.body}</PostBody>
+				<PostBody>{props?.content}</PostBody>
 			</article>
 			<LatestPosts />
 		</main>
